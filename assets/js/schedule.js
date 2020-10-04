6aef32071a168e3d9d4150aa25c13996f28a74ee
kaanistemi <kaanistemi1@gmail.com>
2020-10-04 12:47:22 +0000

    ara guncel

---
+++ assets/js/schedule.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    var defaultTab = $(".daysLink.active").attr("link-day");
+    $("#" + defaultTab).addClass("active");
+
+    $(".daysLink").click(function(){
+        var day = $(this).attr("link-day");
+
+        $(".daysLink").removeClass("active");
+        $(this).addClass("active");
+
+        $(".scheduleTab").removeClass("active");
+        $("#" + day).addClass("active");
+        $( "#" + day ).children().addClass("active");
+    });
+});
+
+// initializing aos animation
+$(document).ready(function(){
+    AOS.init({
+        offset: 200,
+        delay: 100,
+        duration: 500
+    });
+});
