c98abcc0d34f134ae06221f528f2121d5936c70b
kaanistemi <kaanistemi1@gmail.com>
2021-06-15 09:02:33 +0000

    sali guncel

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -24,3 +24,27 @@ $(document).ready(function(){
 });
 
 // MDB Lightbox Init
+$(document).ready(function(){
+    var defaultTab = $(".daysLink-2.active").attr("link-day");
+    $("#" + defaultTab).addClass("active");
+
+    $(".daysLink-2").click(function(){
+        var day = $(this).attr("link-day");
+
+        $(".daysLink-2").removeClass("active");
+        $(this).addClass("active");
+
+        $(".scheduleTab").removeClass("active");
+        $("#" + day).addClass("active");
+        $( "#" + day ).children().addClass("active");
+    });
+});
+
+// initializing aos animation
+$(document).ready(function(){
+    AOS.init({
+        offset: 200,
+        delay: 100,
+        duration: 500
+    });
+});
